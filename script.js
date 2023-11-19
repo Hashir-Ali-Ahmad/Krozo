52a1277f5af7a48cc78554c9776b2d4d313a66b2
Hashir Ali Ahmad <hashir.47a@gmail.com>
2023-11-19 12:14:07 +0500

    Carousel and Recent Articles

--- script.js
+++ script.js
@@ -48,3 +48,21 @@ document.getElementById('product').addEventListener('click',function(e){
   Bcard.style.display = 'none';
   motionGraphy.style.display = 'none';
 });
+
+// Carousel
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 2,
+      },
+    },
+  });
